06ae31b9d21823b86014003c959a3fe1c55af51f
dwanjau <dwanjau@byupathway.edu>
2025-03-06 18:10:31 +0300

    update

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,5 @@
+// Update current year
+document.getElementById('currentyear').textContent = new Date().getFullYear();
+
+// Update last modified date
+document.getElementById('lastModified').textContent = `Last Updated: ${document.lastModified}`;
